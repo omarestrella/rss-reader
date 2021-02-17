bce57ef78fe2674fde1e4c4725162809bdb38472
Omar Estrella <oyestrella91@gmail.com>
2021-02-16 21:17:49 -0500

    Initial Commit

---
+++ Shared/RSSReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSSReaderApp.swift
+//  Shared
+//
+//  Created by Omar Estrella on 2/16/21.
+//
+
+import SwiftUI
+
+@main
+struct RSSReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
